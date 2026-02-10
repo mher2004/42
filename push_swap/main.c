747e6fb249ef3059242edef34aa3e13e014cbc95
Mher Mkrtchyan <mmkrtchy@c2r2s2.42yerevan.am>
2026-02-10 22:32:38 +0400

    Checker full finished

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: mmkrtchy <mmkrtchy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 20:21:42 by mmkrtchy          #+#    #+#             */
-/*   Updated: 2026/02/10 21:39:32 by mmkrtchy         ###   ########.fr       */
+/*   Updated: 2026/02/10 22:31:19 by mmkrtchy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@
 int main(int argc, char ** argv)
 {
     int mode;
-
-    printf("%s\n", argv[1] );
+    
     if (error_checker(argv, argc))
         return (write(1, "Error\n", 6), 0);
     mode = mode_check(argv[1]);
