09c8933a76002839ff63b3e61a385c26815b6cd6
mher2004 <mhermkrtchyan2004@gmail.com>
2026-03-01 18:11:21 +0400

    Some modifications in module00

--- Python modules/module 00/ex2/ft_harvest_total.py
+++ Python modules/module 00/ex2/ft_harvest_total.py
@@ -1,3 +1,4 @@
 def ft_harvest_total():
-    a,b,c = input("Day 1 harvest: "),input("Day 2 harvest: "),input("Day 3 harvest: ")
-    print("Total harvest:",int(a) + int(b) + int(c))
+    a, b = input("Day 1 harvest: "), input("Day 2 harvest: ")
+    c = input("Day 3 harvest: ")
+    print("Total harvest:", int(a) + int(b) + int(c))
