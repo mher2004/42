eba61201808856c68e55ca4274bceed1d9a1aecb
mher2004 <mhermkrtchyan2004@gmail.com>
2026-03-06 22:08:00 +0400

    Module 02 ex1

--- Python modules/module 00/ex5/ft_count_harvest_iterative.py
+++ Python modules/module 00/ex5/ft_count_harvest_iterative.py
@@ -1,5 +1,5 @@
 def ft_count_harvest_iterative():
     a = int(input("Days until harvest: "))
     for i in range(a):
-        print("Days:", i + 1)
+        print("Day", i + 1)
     print("Harvest time!")
--- Python modules/module 00/ex6/ft_garden_summary.py
+++ Python modules/module 00/ex6/ft_garden_summary.py
@@ -1,4 +1,4 @@
 def ft_garden_summary():
-    a, b = input("Enter garden name: "),
-    input("Enter number of plants ")
+    a = input("Enter garden name: ")
+    b = input("Enter number of plants: ")
     print("Garden:", a, "\nPlants:", b, "\nStatus: Growing well!")
