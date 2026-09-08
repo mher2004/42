70fbfd3de6a671adf8c529321f49c5cefc27ac49
Mher <mhermkrtchyan2004@gmail.com>
2026-09-09 00:02:02 +0400

    Added some parts of the logic and some commented schemes to implement later

--- flyin/src/model/graph.py
+++ flyin/src/model/graph.py
@@ -18,7 +18,6 @@ def move_cost(zone: Zone) -> int:
 
 def shortest_path(start: Zone, end: Zone, graph: Graph) -> list[Zone] | None:
     queue: list[tuple[int, str]] = [(0, start.name)]
-    current_dist, current_name = heapq.heappop(queue)
     distances: dict[str, int] = {
         zone_name: float("inf") for zone_name in graph.zones}
     distances[start.name] = 0
@@ -26,4 +25,22 @@ def shortest_path(start: Zone, end: Zone, graph: Graph) -> list[Zone] | None:
     came_from: dict[str, str | None] = {
         zone_name: None for zone_name in graph.zones}
     came_from[start.name] = None
+    while queue:
+        current_dist, current_name = heapq.heappop(queue)
+        if current_dist > distances[current_name]:
+            continue
+        if current_name == end.name:
+            break
+            current_zone = graph.zones[current_name]
+        for conn in current_zone.connections:
+            neighbor = conn.other(current_zone)
+            '''
+            compute current_dist + move_cost(neighbor), 
+            compare against distances[neighbor.name], 
+            and if better, update both dicts and push to the queue.
+            '''
+        
+        #Add the reconstraction path algorithm
+        #Just going backward the came_from[end.name]
+        #untill u reach the end.start fill up all in a list and return it
 
